1211f2969ed8d7f1406be09a6adf2ce82a1943ca
Loc Le <lpl212757@gmail.com>
2024-09-26 20:10:00 +0700

    feat - update blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,13 +1,10 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
-    event = "User FilePost",
-    opts = {
-        indent = { char = "│", highlight = "IblChar" },
-        scope = { char = "│", highlight = "IblScopeChar" },
-    },
-    config = function(_, opts)
-        local hooks = require "ibl.hooks"
-        hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-        require("ibl").setup(opts)
-    end,
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+    config = function()
+        require("ibl").setup()
+    end
 }
