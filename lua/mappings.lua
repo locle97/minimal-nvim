bb2abed387e392b6ebed2e5b1edbd70f7f47152d
Loc Le <lpl212757@gmail.com>
2024-09-26 17:09:20 +0700

    Init

---
+++ lua/mappings.lua
@@ -0,0 +1,161 @@
+local map = vim.keymap.set
+
+map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "move end of line" })
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-l>", "<Right>", { desc = "move right" })
+map("i", "<C-j>", "<Down>", { desc = "move down" })
+map("i", "<C-k>", "<Up>", { desc = "move up" })
+
+map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
+
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "General Save file" })
+map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "General Copy whole file" })
+
+map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "Toggle line number" })
+map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "Toggle relative number" })
+map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "Toggle nvcheatsheet" })
+
+map("n", "<leader>fm", function()
+  require("conform").format { lsp_fallback = true }
+end, { desc = "General Format file" })
+
+-- global lsp mappings
+map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP Diagnostic loclist" })
+
+-- tabufline
+map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
+
+map("n", "<tab>", function()
+  require("nvchad.tabufline").next()
+end, { desc = "buffer goto next" })
+
+map("n", "<S-tab>", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "buffer goto prev" })
+
+map("n", "<leader>x", function()
+  require("nvchad.tabufline").close_buffer()
+end, { desc = "buffer close" })
+
+-- Comment
+map("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "Toggle comment", remap = true })
+
+-- nvimtree
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
+
+-- telescope
+map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
+map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
+map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
+map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
+map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
+map("n", "<leader>th", "<cmd>Telescope themes<CR>", { desc = "telescope nvchad themes" })
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
+map(
+  "n",
+  "<leader>fa",
+  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+  { desc = "telescope find all files" }
+)
+
+-- terminal
+map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
+
+-- new terminals
+map("n", "<leader>h", function()
+  require("nvchad.term").new { pos = "sp" }
+end, { desc = "terminal new horizontal term" })
+
+map("n", "<leader>v", function()
+  require("nvchad.term").new { pos = "vsp" }
+end, { desc = "terminal new vertical window" })
+
+-- toggleable
+map({ "n", "t" }, "<A-v>", function()
+  require("nvchad.term").toggle { pos = "vsp", id = "vtoggleTerm" }
+end, { desc = "terminal toggleable vertical term" })
+
+map({ "n", "t" }, "<A-h>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal toggleable horizontal term" })
+
+map({ "n", "t" }, "<A-i>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
+
+-- whichkey
+map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
+
+map("n", "<leader>wk", function()
+  vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
+end, { desc = "whichkey query lookup" })
+
+-- blankline
+map("n", "<leader>cc", function()
+  local config = { scope = {} }
+  config.scope.exclude = { language = {}, node_type = {} }
+  config.scope.include = { node_type = {} }
+  local node = require("ibl.scope").get(vim.api.nvim_get_current_buf(), config)
+
+  if node then
+    local start_row, _, end_row, _ = node:range()
+    if start_row ~= end_row then
+      vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { start_row + 1, 0 })
+      vim.api.nvim_feedkeys("_", "n", true)
+    end
+  end
+end, { desc = "blankline jump to current context" })
+
+-- add yours here
+
+local map = vim.keymap.set
+local unmap = vim.keymap.del
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- n, v, i, t = mode names
+
+-- Lspconfig Code action
+map("n", "<C-y>", function() vim.lsp.buf.code_action() end , { desc = "lsp code_action", })
+map("n", "<C-.>", function() vim.lsp.buf.code_action() end , { desc = "lsp code_action", })
+
+-- User config
+-- LSPConfig
+map("n", "gD", function() vim.lsp.buf.declaration() end, { desc = "lsp declaration", noremap=true, silent=true })
+map("n", "gd", function() vim.lsp.buf.definition() end, { desc = "lsp definition", noremap = true, silent = true })
+map("n", "K", function() vim.lsp.buf.hover() end, { desc = "lsp hover", })
+map("n", "gi", function() require("telescope.builtin").lsp_implementations() end, { desc = "lsp implementation", noremap = true, silent = true })
+map("n", "gr", function() require("telescope.builtin").lsp_references() end, { desc = "lsp references", noremap = true, silent = true})
+map("n", "go", function() require("telescope.builtin").lsp_document_symbols() end, { desc = "lsp document symbols", noremap = true, silent = true})
+map("n", "<C-y>", function() vim.lsp.buf.code_action() end, { desc = "lsp code_action", })
+map("n", "<F2>", function() vim.lsp.buf.rename() end, { desc = "lsp rename", })
+map("n", "<leader>fm", function() vim.lsp.buf.format { async = true } end, { desc = "LSP formatting", })
+map("v", "<leader>fm", function() vim.lsp.buf.format { async = true } end, { desc = "lsp formatting", })
+map("n", "[d", function() vim.diagnostic.goto_prev() end, { desc = "goto prev" })
+map("n", "]d", function() vim.diagnostic.goto_next() end, { desc = "goto_next" })
+map("n", "<leader>q", function() vim.diagnostic.setloclist() end, { desc = "diagnostic setloclist", })
+map("n", "<leader>f", function() vim.diagnostic.open_float() end, { desc = "floating diagnostic", })
+
+map("n", "<leader>q", function() vim.diagnostic.setloclist() end, { desc = "Diagnostic setloclist", })
+map("n", "<C-p>", "<cmd> Telescope find_files <CR>", { desc = "find files" })
+map("n", "<leader>fp", "<cmd> Telescope projects <CR>", { desc = "Project" })
+
+unmap('n', "<Tab>")
+unmap('n', "<S-Tab>")
+unmap('n', "<leader>x")
+
+-- End user config
---
+++ lua/options.lua
@@ -0,0 +1,28 @@
+vim.cmd.colorscheme "catppuccin"
+
+local o = vim.o
+local g = vim.g
+local opt = vim.opt
+
+g.mapleader = " "
+
+g.gitblame_enabled = false
+g.gitblame_date_format = '%r'
+g.gitblame_highlight_group = 'GitBlameMessage'
+
+-- Indenting
+o.expandtab = true
+o.shiftwidth = 4
+o.smartindent = true
+o.tabstop = 4
+
+opt.relativenumber = true
+
+vim.filetype.add({
+  -- Detect and assign filetype based on the extension of the filename
+  extension = {
+    cql = "cypher",
+    cypher = "cypher",
+    resx = "xml",
+  },
+})
