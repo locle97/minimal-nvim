f7afd191b8b184ff8a38c4ac78baa4dd8754db77
Loc Le <lpl212757@gmail.com>
2024-09-30 10:22:30 +0700

    feat - path display as show filename first

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,24 +1,27 @@
 local options = {
-  defaults = {
-    prompt_prefix = "   ",
-    selection_caret = " ",
-    entry_prefix = " ",
-    sorting_strategy = "ascending",
-    layout_config = {
-      horizontal = {
-        prompt_position = "top",
-        preview_width = 0.55,
-      },
-      width = 0.87,
-      height = 0.80,
+    defaults = {
+        prompt_prefix = "   ",
+        selection_caret = " ",
+        entry_prefix = " ",
+        sorting_strategy = "ascending",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+            },
+            width = 0.87,
+            height = 0.80,
+        },
+        mappings = {
+            n = { ["q"] = require("telescope.actions").close },
+        },
+        path_display = {
+            "filename_first"
+        },
     },
-    mappings = {
-      n = { ["q"] = require("telescope.actions").close },
-    },
-  },
 
-  extensions_list = { },
-  extensions = {},
+    extensions_list = {},
+    extensions = {},
 }
 
 return options
