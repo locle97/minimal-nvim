1d6f0dbad7fb482757c68ae34078d6f39682794c
Loc Le <lpl212757@gmail.com>
2024-10-01 02:08:59 +0700

    feat - not use symbol gitsign anymore

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,11 +2,11 @@ return {
     'lewis6991/gitsigns.nvim',
     config = function()
         require('gitsigns').setup {
-            signs = {
-                add       = { text = '' },
-                change    = { text = '' },
-                untracked = { text = '󰥺' },
-            },
+            -- signs = {
+            --     add       = { text = '' },
+            --     change    = { text = '' },
+            --     untracked = { text = '󰥺' },
+            -- },
         }
     end
 }
