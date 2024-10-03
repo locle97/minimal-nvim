e1faaef59946d6503e7cc8263790ad6f5467a238
Loc Le <lpl212757@gmail.com>
2024-10-03 22:36:00 +0700

    feat - remove lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,9 +4,6 @@ return {
     config = function()
         require('lualine').setup {
             options = { theme = 'catppuccin' },
-            sections = {
-                lualine_c = { 'buffers' },
-            }
         }
     end
 }
