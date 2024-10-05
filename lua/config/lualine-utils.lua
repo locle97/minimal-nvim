60d2d149174df2cc2bc7b0b973831c58d5ae50c0
Loc Le <lpl212757@gmail.com>
2024-10-05 16:17:28 +0700

    feat - LSP config

---
+++ lua/config/lualine-utils.lua
@@ -0,0 +1,38 @@
+local M = {}
+local version = vim.version().minor
+
+M.stbufnr = function()
+  return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+end
+
+M.lsp_msg = function()
+  if version < 10 then
+    return ""
+  end
+
+  local msg = vim.lsp.status()
+
+  if #msg == 0 or vim.o.columns < 120 then
+    return ""
+  end
+
+  local spinners = { "", "󰪞", "󰪟", "󰪠", "󰪢", "󰪣", "󰪤", "󰪥" }
+  local ms = vim.uv.hrtime() / 1e6
+  local frame = math.floor(ms / 100) % #spinners
+
+  return spinners[frame + 1] .. " " .. msg
+end
+
+M.lsp = function()
+  if rawget(vim, "lsp") and version >= 10 then
+    for _, client in ipairs(vim.lsp.get_clients()) do
+      if client.attached_buffers[M.stbufnr()] then
+        return (vim.o.columns > 100 and "   LSP ~ " .. client.name .. " ") or "   LSP "
+      end
+    end
+  end
+
+  return ""
+end
+
+return M
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,8 +2,13 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local utils = require('config.lualine-utils')
+
         require('lualine').setup {
             options = { theme = 'catppuccin' },
+            sections = {
+                lualine_x = { utils.lsp_msg, utils.lsp, 'fileformat', 'filetype' },
+            }
         }
     end
 }
