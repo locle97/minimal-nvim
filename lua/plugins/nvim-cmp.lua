c4cdd7823ca175cff04891737a9262e468020e06
Loc Le <lpl212757@gmail.com>
2024-10-07 03:35:04 +0700

    feat - using lsp signature help

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -34,7 +34,8 @@ return {
             "hrsh7th/cmp-nvim-lua",
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path"
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp-signature-help"
         },
     },
     opts = function()
