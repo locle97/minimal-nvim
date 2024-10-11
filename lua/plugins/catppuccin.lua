f98add14a3d0350824b414e5b36f7879fcf28ff1
Loc Le <lpl212757@gmail.com>
2024-10-11 17:02:18 +0700

    feat - integrate diagnostic for bufferline

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -11,13 +11,24 @@ return {
                     dark = "mocha",
                 },
                 transparent_background = true,
+                integrations = {
+                    bufferline = true
+                },
                 custom_highlights = function(C)
                     return {
+                        -- Cmp Menu
+                        PmenuSel = { fg = C.base, bg = C.maroon, style = { 'bold' } },
+
                         TelescopeSelection = {
                             fg = C.flamingo or C.text,
                             bg = C.surface0 or C.none,
                             style = { "bold" },
                         },
+
+                        -- BufferLine
+                        BufferLineBufferSelected = { fg = C.text, style = { 'bold' } },
+                        BufferLineIndicatorSelected = { fg = C.pink },
+                        BufferLineIndicator = { fg = C.base },
                     }
                 end
             })
