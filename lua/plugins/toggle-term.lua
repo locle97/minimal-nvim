7be5342101151703ff3241ce93e660e1d2fb2152
Loc Le <lpl212757@gmail.com>
2024-10-18 13:35:00 +0700

    feat - default shell

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -2,13 +2,10 @@ return {
     "akinsho/toggleterm.nvim",
     lazy = false,
     config = function()
-        local utility = require("utility")
-
         require("toggleterm").setup {
             open_mapping = [[<c-\>]],
             direction = "float",
             shade_terminals = true,
-            shell = utility.shell,
         }
     end,
 }
