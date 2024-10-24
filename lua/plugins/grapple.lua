934a4c4af3a47274a90cf80658fbf9138f8e9ab1
Loc Le <lpl212757@gmail.com>
2024-10-24 16:34:10 +0700

    reconfig for grapple

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -3,6 +3,7 @@ return {
     keys = {
         { "<leader>mm", "<cmd>Grapple tag<cr>",            desc = "Tag a file" },
         { "<leader>hh", "<cmd>Grapple toggle_tags<cr>",    desc = "Toggle tags menu" },
+        { "<leader><leader>", "<cmd>Grapple toggle_tags<cr>",    desc = "Toggle tags menu" },
         { "<leader>hf", "<cmd>Telescope grapple tags<cr>", desc = "Toggle telescope of tags menu" },
 
         { "<leader>1",  "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
@@ -10,11 +11,10 @@ return {
         { "<leader>3",  "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
         { "<leader>4",  "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
     },
-    config = function()
-        require("grapple").setup({
-            icons = true,  -- setting to "true" requires "nvim-web-devicons"
-            status = true,
-            style = "basename",
-        })
-    end
+    opts = {
+        scope = "git",
+        icons = true,     -- setting to "true" requires "nvim-web-devicons"
+        status = true,
+        style = "basename",
+    }
 }
