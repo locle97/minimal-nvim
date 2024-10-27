7b5138c9b873eb15da660587177308f03e962a7c
Loc Le <lpl212757@gmail.com>
2024-10-27 16:13:05 +0700

    add todo comments and grapple hotkey

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -1,15 +1,14 @@
 return {
     "cbochs/grapple.nvim",
     keys = {
-        { "<leader>mm", "<cmd>Grapple tag<cr>",            desc = "Tag a file" },
-        { "<leader>hh", "<cmd>Grapple toggle_tags<cr>",    desc = "Toggle tags menu" },
+        { "<c-m>", "<cmd>Grapple tag<cr>",            desc = "Tag a file" },
         { "<leader><leader>", "<cmd>Grapple toggle_tags<cr>",    desc = "Toggle tags menu" },
-        { "<leader>hf", "<cmd>Telescope grapple tags<cr>", desc = "Toggle telescope of tags menu" },
 
         { "<leader>1",  "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
         { "<leader>2",  "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
         { "<leader>3",  "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
         { "<leader>4",  "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+        { "<leader>5",  "<cmd>Grapple select index=5<cr>", desc = "Select fifth tag" },
     },
     opts = {
         scope = "git",
