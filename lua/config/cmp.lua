9578ff8dee3449e0821dd9bd63a679560ccf188c
Loc Le <lpl212757@gmail.com>
2024-10-27 17:13:03 +0700

    fix cmp signature

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -71,9 +71,25 @@ local options = {
         ["<C-e>"] = cmp.mapping.close(),
 
         ["<CR>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
+            behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
+
+        -- ["<CR>"] = function(fallback)
+        --     -- Don't block <CR> if signature help is active
+        --     -- https://github.com/hrsh7th/cmp-nvim-lsp-signature-help/issues/13
+        --     if not cmp.visible() or not cmp.get_selected_entry() or cmp.get_selected_entry().source.name == 'nvim_lsp_signature_help' then
+        --         fallback()
+        --     else
+        --         cmp.confirm({
+        --             -- Replace word if completing in the middle of a word
+        --             -- https://github.com/hrsh7th/nvim-cmp/issues/664
+        --             behavior = cmp.ConfirmBehavior.Replace,
+        --             -- Don't select first item on CR if nothing was selected
+        --             select = false,
+        --         })
+        --     end
+        -- end,
 
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
