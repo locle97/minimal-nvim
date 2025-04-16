ceefe6c63097e92b1a2eaebf1ab3f87a765940c1
Loc Le <lpl212757@gmail.com>
2025-04-16 13:53:28 +0700

    remove unused plugins

--- lua/plugins/http-client.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "askfiy/http-client.nvim",
-    config = function()
-        require("http-client").setup()
-    end,
-}
--- lua/plugins/vague.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "vague2k/vague.nvim",
-    config = function()
-        require("vague").setup({
-            transparent = true
-        })
-    end
-}
