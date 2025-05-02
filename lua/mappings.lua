271432c496917ceb8c2b8791ba3b3f4d3dabde2d
Loc Le <lpl212757@gmail.com>
2025-05-02 10:03:16 +0700

    Mapping to toggle instead of focus

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -49,8 +49,8 @@ map("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
 map("v", "<leader>/", "gc", { desc = "Toggle comment", remap = true })
 
 -- nvimtree
-map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
-map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
+-- map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree focus window" })
 -- map("n", "<leader>e", "<cmd>Explore<CR>", { desc = "nvimtree focus window" })
 
 -- telescope
